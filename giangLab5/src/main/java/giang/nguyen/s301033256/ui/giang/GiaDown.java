0613f1c5c6b11d9b553d1b10030ab7bd70f4a088
Giang Nguyen <giangkawaii1999@gmail.com>
2020-11-29 22:49:29 -0500

    WIP: download img fragment (btn + progress bar)

--- giangLab5/src/main/java/giang/nguyen/s301033256/ui/giang/GiaDown.java
+++ giangLab5/src/main/java/giang/nguyen/s301033256/ui/giang/GiaDown.java
@@ -11,12 +11,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import giang.nguyen.s301033256.R;
 
 public class GiaDown extends Fragment {
 
     private GiangViewModel mViewModel;
+
+    View root;
+    ProgressBar loading;
 
     public static GiaDown newInstance() {
         return new GiaDown();
@@ -25,7 +31,18 @@ public class GiaDown extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.gia_down, container, false);
+        root = inflater.inflate(R.layout.gia_down, container, false);
+        Button downloadImgBtn = (Button)root.findViewById(R.id.giangDownloadImgBtn);
+        loading = (ProgressBar)root.findViewById(R.id.progress);
+        downloadImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"BTN WORKING", Toast.LENGTH_SHORT).show();
+                loading.setVisibility(View.VISIBLE);
+            }
+        });
+
+        return root;
     }
 
     @Override
